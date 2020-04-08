84038ad97fea9652ad50477c6c9174b8aeea6f64
Jacob Carlone <carlone.jacob@gmail.com>
2020-04-08 01:15:18 +0000

    created a function to store the original code in that outputs each character of a string with a delay

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,15 @@
-const sentence = "hello there from lighthouse labs";
-
-for (const char of sentence) {
-  let timer = 0;
-  setTimeout(() => {
-    process.stdout.write(char);
-  }, timer)
-  timer += 50;
-}
-setTimeout(() => {
-  console.log();
-},0);
-
+const typewriter = function(sentence) {
+  for (const char of sentence) {
+     let timer = 0;
+     setTimeout(() => {process.stdout.write(char);
+     }, timer);    
+     timer += 50;
+   }
+   // process.stdout.write("\n")
+   setTimeout(() => {
+     console.log();
+   }, 0);
+ 
+ }
+ const sentence = 'Hello from lighthouse labs';
+ typewriter(sentence);
